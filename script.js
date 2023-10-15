1e7b8dffd1c5c3546844a0155ad29dfd1b0e53c7
Vinay R <144917498+vinayr002@users.noreply.github.com>
2023-10-15 17:46:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('nav a');
+
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
+
+const toggleButton = document.getElementById('nav-toggle')
+const navlinks =  document.getElementById('nav-links')
+
+toggleButton.addEventListener('click', () => {
+    navlinks.classList.toggle('active');
+})
+navlinks.addEventListener('click',()=>{
+    navlinks.classList.remove('active');
+})
